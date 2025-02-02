9257c5f0f2ddccde30df0418bd5045a3623ec5a5
nono <nonokub.671@gmail.com>
2025-02-02 08:25:55 +0700

    add console log when unsub

--- frontend/hooks/useWebsocket.ts
+++ frontend/hooks/useWebsocket.ts
@@ -27,6 +27,7 @@ export const useWebSocket = () => {
   const unsubscribe = (subscription: Stomp.Subscription | undefined) => { 
     if (subscription) {
       subscription.unsubscribe();
+      console.log(`Unsubscribed from ${subscription.id}`);
     } else {
       console.log("No active WebSocket connection to disconnect.");
     }
